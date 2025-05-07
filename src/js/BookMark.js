37db5b6d188ff11abcd714bb0eae08a922cbbb31
Quema <myoon0929@gmail.com>
2025-05-08 00:57:38 +0900

    fix script

--- src/js/BookMark.js
+++ src/js/BookMark.js
@@ -1,35 +1,42 @@
 chrome.tabs.query({ active: true }, ([tab]) => {
-	const isVideo = /youtube\.com\/(watch|shorts)/.test(tab.url);
-	const addBookmark = document.getElementById('addBookmark');
-	if (addBookmark) {
-		addBookmark.disabled = !isVideo;
-	}
+    const isVideo = /youtube\.com\/(watch|shorts)/.test(tab.url);
+    const addBookmark = document.getElementById('addBookmark');
+    if (addBookmark) {
+        addBookmark.disabled = !isVideo;
+    }
 })
 
 const bookmark = () => {
-	const addBookmark = document.getElementById('addBookmark')
+    const addBookmark = document.getElementById('addBookmark')
 
-	addBookmark.addEventListener('click', async () => {
-		let [tab] = await chrome.tabs.query({ active: true });
-		if (!/youtube\.com\/(watch|shorts)/.test(tab.url)) return console.log(!/youtube\.com\/(watch|shorts)/.test(tab.url));
-		chrome.scripting.executeScript({
-			target: { tabId: tab.id },
-			func: () => {
-				const titleElement = document.querySelector('h1.title.style-scope.ytd-video-primary-info-renderer');
-				const [href] = location.href.split('&')
-				if (titleElement) {
-					const title = titleElement.textContent.trim();
-					console.log('YouTube Video Title:', title);
-					return { title: title, url: href };
-				} else {
-					console.log('Not found title');
-					return { title: null, url: href };
-				}
+    addBookmark.addEventListener('click', async () => {
+        let [tab] = await chrome.tabs.query({ active: true });
+        if (!/youtube\.com\/(watch|shorts)/.test(tab.url)) return console.log(!/youtube\.com\/(watch|shorts)/.test(tab.url));
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            func: () => {
+                const titleElement = document.querySelector('h1.title.style-scope.ytd-video-primary-info-renderer');
+                const shortstitleElement = document.querySelector('h2.ytShortsVideoTitleViewModelShortsVideoTitle');
+                const [href] = location.href.split('&');
 
-			}
-		}, ([res]) => {
-			console.log(res)
-			if (res?.result) saveItem('bookmarks', res.result);
-		});
-	});
+                const title = titleElement ? titleElement.textContent.trim() : null;
+                const shortstitle = shortstitleElement ? shortstitleElement.textContent.trim() : null;
+
+                if (title) {
+                    console.log('YouTube Video Title:', title);
+                    return { title: title, url: href };
+                } else if (shortstitle) {
+                    console.log('YouTube Shorts Video Title:', shortstitle);
+                    return { title: shortstitle, url: href };
+                } else {
+                    console.log('Not found title');
+                    return { title: null, url: href };
+                }
+
+            }
+        }, ([res]) => {
+            console.log(res)
+            if (res?.result) saveItem('bookmarks', res.result);
+        });
+    });
 }
